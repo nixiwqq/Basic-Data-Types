4042673ec7485b8d09478a464e736bb24c35bac7
Nika <129578493+nixiwqq@users.noreply.github.com>
2025-09-18 22:23:44 +0200

    Create Datatypes.cpp
    Sample case 0

---
+++ Datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namesace std;
+
+int main() {        \\*Test case 0
+  int s = 3;
+  long f = 12345678912345;
+  char ch = 'a';
+  float d = 334.230;
+  double ka = 14049.304930000;
+  printf("%d\n%ld\n%c\n%.3f\n%.9lf\n", s , f, ch, d, ka);
+
+  return 0;
+}
+    
